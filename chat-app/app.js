51bdd7dad2c5b97054fb5199321e9d59195d41fc
superman1006 <1402788264@qq.com>
2023-04-19 00:59:57 +0800

    1

--- chat-app/app.js
+++ chat-app/app.js
@@ -1,86 +1,62 @@
 const chatForm = document.getElementById('chat-form');
-const chatInput = document.getElementById('chat-input');
-const chatMessages = document.getElementById('chat-messages');
-const status = document.getElementById('status');
-
-// 建立连接
+const messageContainer = document.getElementById('messages-container');
 const socket = io();
 
-// 监听连接成功事件
-socket.on('connect', () => {
-  status.innerText = 'Connected';
+// Output message to DOM
+function outputMessage (message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+                    <p class="text">${message.text}</p>`;
+  messageContainer.appendChild(div);
+}
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
 });
 
-// 监听新用户加入聊天室事件
-socket.on('user joined', (username) => {
-  const message = `${username} has joined the chat`;
-  const li = document.createElement('li');
-  li.classList.add('system');
-  li.innerText = message;
-  chatMessages.appendChild(li);
+// Join chatroom
+socket.emit('joinRoom', { username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
 });
 
-// 监听用户发送消息事件
-chatForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const message = chatInput.value;
-  socket.send('chat message', message);
-  chatInput.value = '';
+// Message from server
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message);
+  // Scroll down to the latest message
+  messageContainer.scrollTop = messageContainer.scrollHeight;
 });
 
-// 监听用户收到新消息事件
-socket.on('chat message', (data) => {
-  const { username, message } = data;
-  const li = document.createElement('li');
-  li.innerText = `${username}: ${message}`;
-  chatMessages.appendChild(li);
+// Message submit
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  // Get message text
+  let msg = e.target.elements.msg.value;
+  msg = msg.trim();
+  if (!msg) {
+    return false;
+  }
+  // Emit message to server
+  socket.emit('chatMessage', msg);
+  // Clear input field
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
 });
 
-// 监听用户离开聊天室事件
-socket.on('user left', (username) => {
-  const message = `${username} has left the chat`;
-  const li = document.createElement('li');
-  li.classList.add('system');
-  li.innerText = message;
-  chatMessages.appendChild(li);
-});
+// Output room name
+function outputRoomName (room) {
+  const roomName = document.getElementById('room-name');
+  roomName.innerText = room;
+}
 
-
-//将 Express 和 Socket.IO 模块加载到你的项目中，并将其应用于 Express 应用程序
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app); // 使用 createServer 方法创建一个 http 服务器
-const io = require('socket.io')(http);
-
-app.use(express.static(__dirname + '/public'));
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-  socket.on('chat message', (data) => {
-    const { username, message } = data;
-    console.log('message: ' + message);
-    io.emit('chat message', { username, message });
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
-});
-
-// 添加以下代码来处理 POST 请求
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// 用 module.exports 导出 app 实例
-module.exports = app;
-
-// 以便 Express 能够正确地解析静态文件
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Output users
+function outputUsers (users) {
+  const userList = document.getElementById('users');
+  userList.innerHTML = `${users.map(user => `<li>${user.username}</li>`).join('')}`;
+}
