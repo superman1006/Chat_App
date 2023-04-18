210534d18a180f784cb29d2924f39c3748f8dda9
superman1006 <1402788264@qq.com>
2023-04-19 00:48:52 +0800

    1

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const app = express();
 const http = require('http');
+const app = require('./app');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
