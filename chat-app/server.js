579f8410e6a5533a91183fc603d408ca459936c3
superman1006 <1402788264@qq.com>
2023-04-20 13:54:10 +0800

    1

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,47 +1,29 @@
 const express = require('express');
-const http = require('http');
-const app = require('./app');
-const server = http.createServer(app);
-const io = require('socket.io')(server);
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+// Serve static files
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
+// Handle socket.io connections
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  // 监听新用户加入聊天室事件
-  socket.on('user joined', (username) => {
-    socket.broadcast.emit('user joined', username);
+  // Listen for chat messages
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg); // Broadcast the message to all connected clients
   });
 
-  // 监听用户发送消息事件
-  socket.on('chat message', (message) => {
-    const username = socket.handshake.query.username;
-    io.emit('chat message', { username, message });
-  });
-
-  // 监听用户离开聊天室事件
+  // Handle disconnections
   socket.on('disconnect', () => {
     console.log('user disconnected');
-    const username = socket.handshake.query.username;
-    socket.emit('user left', username);
   });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
-
-app.get('/stylesheets/style.css', function (req, res) {
-  res.type('text/css');
-  res.sendFile(__dirname + '/public/stylesheets/style.css');
-});
-
+// Start the server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
 });
