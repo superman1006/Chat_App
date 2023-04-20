0596fb8ff6245b71e32cf544bbbf759c59f2b2ae
superman1006 <1402788264@qq.com>
2023-04-20 11:07:05 +0800

    1

--- chat-app/public/reference/index.js
+++ chat-app/public/reference/index.js
@@ -1,35 +1,3 @@
-// 获取导航栏中的三个单词
-const home = document.querySelector('.navbar-nav .nav-item:nth-child(1) .nav-link');
-const about = document.querySelector('.navbar-nav .nav-item:nth-child(2) .nav-link');
-const chat = document.querySelector('.navbar-nav .nav-item:nth-child(3) .nav-link');
-
-// 获取横条元素
-const activeIndicator = document.createElement('div');
-activeIndicator.classList.add('active-indicator');
-
-
-// 页面加载时更新横条的位置和颜色
-updateActiveIndicatorPosition();
-
-// 当用户鼠标移动到单词时，更新横条的位置和颜色
-home.addEventListener('mouseover', () => {
-  activeIndicator.classList.remove('active-about', 'active-chat');
-  activeIndicator.classList.add('active-home');
-  updateActiveIndicatorPosition();
-});
-
-about.addEventListener('mouseover', () => {
-  activeIndicator.classList.remove('active-home', 'active-chat');
-  activeIndicator.classList.add('active-about');
-  updateActiveIndicatorPosition();
-});
-
-chat.addEventListener('mouseover', () => {
-  activeIndicator.classList.remove('active-home', 'active-about');
-  activeIndicator.classList.add('active-chat');
-  updateActiveIndicatorPosition();
-});
-
 
 const io = require('socket.io')(server);
 
