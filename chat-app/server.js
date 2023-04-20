78e234d89ea6af2a1b376479d65cedc79a05be13
superman1006 <1402788264@qq.com>
2023-04-20 17:09:35 +0800

    1

--- chat-app/server.js
+++ chat-app/server.js
@@ -23,7 +23,6 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
