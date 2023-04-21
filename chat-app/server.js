50cf32018550d1a7c8661842e52cb70f0e233942
superman1006 <1402788264@qq.com>
2023-04-21 19:06:34 +0800

    1

--- chat-app/server.js
+++ chat-app/server.js
@@ -2,7 +2,7 @@
  * @Author: superman1006 1402788264@qq.com
  * @Date: 2023-04-18 22:15:14
  * @LastEditors: superman1006 1402788264@qq.com
- * @LastEditTime: 2023-04-21 16:14:21
+ * @LastEditTime: 2023-04-21 18:23:40
  * @FilePath: \chat\chat-app\server.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -38,7 +38,7 @@ io.on('connection', (socket) => {
     // Broadcast a message to all other connected clients
     socket.broadcast.emit('chat message', 'a user has disconnected');
   });
-  
+
 
   // Handle user left
   socket.on('disconnect', (reason) => {
